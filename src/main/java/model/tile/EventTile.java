ec9dc347ec0b5972254986bdf4f80d7665706497
axel.dumon.etu <axel.dumon.etu@univ-lille.fr>
2024-01-22 10:30:53 +0100

    map

---
+++ src/main/java/model/tile/EventTile.java
@@ -0,0 +1,13 @@
+package model.tile;
+
+public class EventTile extends Tile{
+    public EventTile(){
+        super("Event", 0.2);
+    }
+
+    @Override
+    public void doTile() {
+                
+    }
+
+}
